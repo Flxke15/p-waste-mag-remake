c979cea73bd12520de3b92e1c5186f6acc3d8adf
F1uke 🐻 <f1uke@Flxke-macbook.local>
2023-06-22 17:04:45 +0700

    change database and addAdmin pages in Owner

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect, Component} from "react";
+import axios from "axios";
+import Navbar from "./component/navbar";
+import Login from "./Pages/login";
+
+function App() {
+  return (
+      <Login/>
+  );
+}
+
+export default App;
